bcf37ebd374084d38dcec6f6eb764f438498f891
allabe93 <111697941+allabe93@users.noreply.github.com>
2022-11-06 15:57:01 +0100

    I am pushing lab-intro-sql

---
+++ lab-intro-sql.sql
@@ -0,0 +1,15 @@
+select * from actor;
+
+select * from film;
+
+select * from customer;
+
+select title from film;
+
+select name as language from language;
+
+select count(store_id) as Number_of_Stores from store;
+
+select count(first_name) as Headcount from staff;
+
+select first_name from staff;
